b082cf160df085a9409f335466cb3929dfc0af3d
Jacob Zhong <cmpute@qq.com>
2023-01-20 18:44:34 -0500

    Implemented the style for tabs

--- assets/ts/tabs.ts
+++ assets/ts/tabs.ts
@@ -1,6 +1,5 @@
 
-function switchTab(e: PointerEvent) {
-    const btn = e.target as HTMLLIElement;
+function switchTab(btn: HTMLLIElement) {
     const tabGroup = btn.getAttribute("data-tab-group");
     const tabId = btn.getAttribute("data-tab-item");
     const allTabItems = document.querySelectorAll("[data-tab-group='"+tabGroup+"']");
@@ -9,19 +8,19 @@ function switchTab(e: PointerEvent) {
     // save button position relative to viewport
     var yposButton = btn.scrollTop;
 
-    allTabItems.forEach(item => item.classList.remove("active"));
-    targetTabItems.forEach(item => item.classList.add("active"));
+    allTabItems.forEach(item => item.classList.remove("active-class"));
+    targetTabItems.forEach(item => item.classList.add("active-class"));
 
     // reset screen to the same position relative to clicked button to prevent page jump
     var yposButtonDiff = btn.scrollTop - yposButton;
     window.scrollTo(window.scrollX, window.scrollY + yposButtonDiff);
 }
 
-const navButtonsQuery = "li.tab-nav-button";
+const navButtonsQuery = "li.tab-nav-item";
 
 function setupTabSwitchers() {
     document.querySelectorAll(navButtonsQuery).forEach(btn => {
-        btn.addEventListener("click", switchTab);
+        btn.addEventListener("click", e => switchTab(btn as HTMLLIElement));
     });
 }
 
