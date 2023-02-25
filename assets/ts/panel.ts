8a1bb165e965578b1d59bd00055d7fa75e66a8e4
Jacob Zhong <cmpute@qq.com>
2023-02-24 22:55:24 -0500

    Refactor panel to use details tag

--- assets/ts/panel.ts
+++ assets/ts/panel.ts
@@ -1,47 +1,34 @@
 
-function togglePanel(panel: HTMLDivElement, body_selector: string, offset) {
-    const body = panel.querySelector(body_selector) as HTMLDivElement;
-    if (panel.classList.contains("active-class")) {
-        panel.classList.remove("active-class");
-        body.style.height = "0";
-    } else {
-        panel.classList.add("active-class");
-
-        const contents = body.querySelector("section");
-        body.style.height = contents.clientHeight + offset + "px";
-    }
-}
-
 function setupPanelToggles() {
-    // register inline toc
+    // register closing animation of inline toc
     const toc = document.getElementById("TableOfContents-inline");
     if (toc) {
-        const toc_panel = toc.parentElement.parentElement.parentElement;
-        const toc_title = toc_panel.querySelector("div.article-toc-title");
-
-        // set an initial value so that the animation works at the first time
-        const toc_contents = toc_panel.querySelector("section");
-        const height = toc_contents.clientHeight + 32 + "px";
-        toc_contents.parentElement.style.height = height; 
-
-        toc_title.addEventListener("click", e => togglePanel(
-            toc_panel as HTMLDivElement, "div.article-toc-body", 32
-        ));
+        const toc_panel = toc.parentElement.parentElement;
+        toc_panel.addEventListener("click", function(e) {
+            if (toc_panel.hasAttribute("open")) {
+                e.preventDefault();
+                toc_panel.classList.add("closing");
+                setTimeout(() => {
+                    toc_panel.removeAttribute("open");
+                    toc_panel.classList.remove("closing");
+                }, 300); // animation in css is 400ms, set smaller to make sure closing before animation finished
+            }
+        });
     }
 
-    // register notice panels
-    document.querySelectorAll("div.article-notice-title").forEach(title => {        
-        // set an initial value so that the animation works at the first time
-        const panel = title.parentElement as HTMLDivElement;
-        const contents = panel.querySelector("section");
-
-        if (panel.classList.contains("active-class")) {
-            const height = contents.clientHeight + "px";
-            contents.parentElement.style.height = height;
-        }
-
-        title.addEventListener("click", e => togglePanel(panel, "div.article-notice-body", 0));
-    })
+    // register closing animation of notice panels
+    document.querySelectorAll("details.article-notice").forEach(notice => {
+        notice.addEventListener("click", function(e) {
+            if (notice.hasAttribute("open")) {
+                e.preventDefault();
+                notice.classList.add("closing");
+                setTimeout(() => {
+                    notice.removeAttribute("open");
+                    notice.classList.remove("closing");
+                }, 300);
+            }
+        });
+    });
 }
 
 export { setupPanelToggles };
